a48c244844e029ba8bb7d02438d672266069c3e2
拓海 前田 <190093@jc-21.jp>
2022-02-15 14:16:30 +0900

    プロジェクト ファイルを追加します。

---
+++ DirectedE.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace wagak
+{
+    public class DirectedE
+    {
+        public static int SEARCH(int a)
+        {
+            return 0;
+        }
+    }
+}
---
+++ kuriage11.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace wagak
+{
+    public class kuriage11
+    {
+        public static int kuriage22(int i)
+        {
+            return 0;
+        }
+    }
+}
